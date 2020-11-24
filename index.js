48a6f8c30827dd7e22059cb82dd21f5ad572b02b
Soldy <4786022+Soldy@users.noreply.github.com>
2020-11-24 08:06:51 +0000

    rethink
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,43 +1,81 @@
-
+/*
+ *  @Soldy\logrc\2020.11.24\GPL3
+ */
 const fs = require('fs');
 
 
 const logBase = function(logFileNameIn){
+    /*
+     * @param {JSONobject}
+     * @public
+     * @return {boolean}
+     */
     this.log = function(object){
-        log = object;
+        logs.push({
+            time : (+new Date),
+            data : clean(object)
+        });
         write();
-    }
-    this.add = function(name, value){
-        log[name]=value;
-        return true;
-    }
+    };
+    /*
+     * @public
+     * @return {boolean}
+     */
     this.write = function(){
-        write();
-    }
+        return write();
+    };
+    /*
+     * @private
+     * @var {boolean}
+     */
     let writing = false;
-    let log = {};
+    /*
+     * @private
+     * @var {array}
+     */
+    let logs = [];
+    /*
+     * @private
+     * @var {string}
+     */
     let logFile = logFileNameIn;
+    /*
+     * @private
+     * @return {boolean}
+     */
     let write = async function (){
+        if(writing)
+            return true;
+        if(1 > logs.length)
+            return false;
         writing = true;
-        log.timestamp = (+new Date);
         fs.appendFile(
             logFile,
-            log,
+            line(),
             function(){
-                log={};
                 writing = false;
+                if(logs.length > 0)
+                    return write();
+                return true;
             }
-         );
+        );
     };
-    let prepare = function(message, subject){
-        if (typeof message !== 'string')
-            return false;
-        let new_log = {
-            'timestamp':(+new Date)
-        }
-        if (typeof subject === 'string')
-            new_log['subject']=subject.replace(/[\r\n]+/gm, "");
-        new_log.message = message.replace( /[\r\n]+/gm, ""); 
-        return JSON.stringify(new_log);
+    let line = function(){
+        return JSON.stringify(
+                logs.shift()
+            )+'\n';
+    }
+    /*
+     * @param {JSONobject}
+     * @private
+     * @return {JSONobject}
+     *
+     */
+    let clean = function(object){
+        return JSON.parse(
+            JSON.stringify(object).replace( /[\r\n]+/gm, "")
+        );
     };
-}
+};
+
+exports.logBase = logBase;
